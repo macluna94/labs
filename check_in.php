9f4803fc492138cdd12724da730ff8ef68e22c37
macluna94 <macluna94@gmail.com>
2019-09-24 15:17:31 -0500

    v0.01

---
+++ check_in.php
@@ -0,0 +1,26 @@
+<?php
+
+$servername = "localhost";
+$database = "Lab";
+$username = "root";
+$pass = "";
+$table = "clases";
+
+$connection = mysqli_connect(
+    $servername,
+    $username,
+    $pass,
+    $database
+);
+
+$codigo = $_POST['codigo'];
+
+
+
+
+
+
+
+
+
+?>
